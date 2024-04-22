cc8c69b9c1a265fa569587918229bae8f85a63b3
Juliano <kluge233@hotmail.com>
2024-04-21 22:42:16 -0300

    Criação da Home Page

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,11 +12,35 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculadora IMC',),centerTitle: true,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Calculadora IMC',),centerTitle: true,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(8),
+          child: const Column(
+            children: [
+             TextField(
+               decoration: InputDecoration(
+                 border: OutlineInputBorder(
+                 ),
+                 hintText: 'PESO',
+      
+               ),
+               keyboardType: TextInputType.number,
+             ),
+              SizedBox(height: 8,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'ALTURA'
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-      body: ,
     );
   }
 }
